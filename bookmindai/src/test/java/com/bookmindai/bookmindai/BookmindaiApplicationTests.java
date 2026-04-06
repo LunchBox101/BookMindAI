0324789d7f88da7009aa6e4b6a13a948e16c068e
LunchBox101 <lancetleonard@gmail.com>
2026-04-06 18:49:35 -0400

    Initial project setup - Spring Boot 4.0.5 + Java 21

---
+++ bookmindai/src/test/java/com/bookmindai/bookmindai/BookmindaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmindai.bookmindai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmindaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
